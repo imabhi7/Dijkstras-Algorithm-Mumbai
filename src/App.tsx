66b25e9dd3fc741d26b402913c8697857df49b32
Abhinav Barman <abhinavbarman2002@gmail.com>
2024-11-11 04:04:44 +0530

    pushed react code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MumbaiRailNetwork from './MumbaiRailNetwork';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <MumbaiRailNetwork />
+    </div>
+  );
+};
+
+export default App;
